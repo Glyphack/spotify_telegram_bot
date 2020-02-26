ba5a00293d540e3dd6c9fb26c387097876a186c9
glyphack <sh.hooshyari@cafebazaar.ir>
2020-02-26 11:23:38 +0330

    Add is playlist check.

--- command_hanlders/send_playlist_songs.py
+++ command_hanlders/send_playlist_songs.py
@@ -1,21 +1,20 @@
-import logging
 import uuid
 
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
-from command_hanlders.helpers.spotdl import (
-    download_from_list,
-    send_songs_from_directory,
-    create_download_list_from_link
-)
-
-logger = logging.getLogger(__name__)
+from command_hanlders.helpers import spotdl
 
 
 @run_async
 def send_playlist_songs(update: Update, context: CallbackContext):
     playlist_link = context.args[0]
+    if "playlist" not in playlist_link:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Playlist links must contain playlist in their link."
+        )
+        return
     download_path = str(uuid.uuid4())
     list_path = f"{download_path}.txt"
 
@@ -23,9 +22,9 @@ def send_playlist_songs(update: Update, context: CallbackContext):
         chat_id=update.effective_chat.id, text=f"donwloading playlist"
     )
 
-    create_download_list_from_link(playlist_link, "playlist", list_path)
-    download_from_list(list_path, download_path)
-    send_songs_from_directory(download_path, update, context)
+    spotdl.create_download_list_from_link(playlist_link, "playlist", list_path)
+    spotdl.download_from_list(list_path, download_path)
+    spotdl.send_songs_from_directory(download_path, update, context)
 
     context.bot.send_message(
         chat_id=update.effective_chat.id, text="done"
