d0d5dc0d3d83ce05ba2d454744092a96b427c8be
Shayegan Hooshyari <sh.hooshyari@gmail.com>
2021-12-19 18:03:01 +0330

    Refactor code

--- command_handlers/send_spotify_songs.py
+++ command_handlers/send_spotify_songs.py
@@ -8,12 +8,11 @@ from telegram.ext import CallbackContext
 
 def send_spotify_songs(update: Update, context: CallbackContext):
     song_link = context.args[0]
-    download_path = os.getcwd() + "/" + str(uuid.uuid4())
-
     context.bot.send_message(
         chat_id=update.effective_chat.id, text=f"downloading songs..."
     )
 
+    download_path = os.getcwd() + "/" + str(uuid.uuid4())
     spotdl.download_from_spotify(download_path, song_link)
     spotdl.send_songs_from_directory(download_path, update, context)
 
